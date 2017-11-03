368a70b643a44e91cccc8b94d8e947a0fe6fd044
Henrique Ferreira <henrique.ferreira@nearsoftsolutions.com>
2017-11-03 09:48:19 +0000

    Add website parameter to console input
    Now main.go receives an aditional parameter for website to monitor passed through the console.

--- main.go
+++ main.go
@@ -4,20 +4,23 @@ import "net/http"
 import "fmt"
 import "../notificator"
 import "log"
+import "os"
 
 var notify *notificator.Notificator
 
 func main(){
+	
+	argsWithProg := os.Args[1]
+
 	notify = notificator.New(notificator.Options{
 		DefaultIcon: "icon/default.png",
-		AppName:     "Up-Monitor",
+		AppName:     os.Args[1],
 	})
-	
 	for{
-		resp, err := http.Get("http://localhost:9200")
+		resp, err := http.Get(argsWithProg)
 		if err != nil {
 			notify.Push("DOWN or ERROR", err.Error(), "/Users/henriqueferreira/Documents/GoLangDEV/up-monitor/icon.png", notificator.UR_CRITICAL)
-			break
+			fmt.Println(err.Error());
 		} else {
 			fmt.Println(string(resp.StatusCode) + resp.Status)
 		}
